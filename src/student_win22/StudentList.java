6ae9ac22c69f731201dc4d430f11b26cdf005582
dongeun <dekim1209@gmail.com>
2022-02-08 08:33:31 -0500

    array size changed from 3 to 4

--- src/student_win22/StudentList.java
+++ src/student_win22/StudentList.java
@@ -6,7 +6,7 @@ public class StudentList
 {
    public static void main (String[] args)
    {
-      Student[] studentList = new Student[3];
+      Student[] studentList = new Student[4];
       studentList[0] = new Student("s1", "Ronak Sheth");
       studentList[1] = new Student("s2", "John Doe");
       studentList[2] = new Student("s3", "Abc");
